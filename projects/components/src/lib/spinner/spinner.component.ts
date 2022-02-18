e4a6005c0fce99a4e0785bb80653db50147577fd
ruifang <ruifang.li09@gmail.com>
2022-02-18 12:59:09 -0700

    clean up

--- projects/components/src/lib/spinner/spinner.component.ts
+++ projects/components/src/lib/spinner/spinner.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SpinnerComponent  {
 
   @Input() size: 'medium' | 'large' = 'medium';
+  @Input() type: 'default' | 'inverse' = 'default';
   constructor() { }
 
 }
