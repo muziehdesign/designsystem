243b126f8ded905b4e7787c1d52895adaae656cb
ruifang <ruifang.li09@gmail.com>
2022-02-19 12:27:59 -0700

    partial modal

--- src/app/design/modal-guidelines/modal-guidelines.component.ts
+++ src/app/design/modal-guidelines/modal-guidelines.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { ModalAddOrderComponent } from '../modal-add-order/modal-add-order.component';
 
 @Component({
-  selector: 'app-modal-guidelines',
-  templateUrl: './modal-guidelines.component.html',
-  styleUrls: ['./modal-guidelines.component.scss']
+    selector: 'app-modal-guidelines',
+    templateUrl: './modal-guidelines.component.html',
+    styleUrls: ['./modal-guidelines.component.scss'],
 })
-export class ModalGuidelinesComponent implements OnInit {
+export class ModalGuidelinesComponent implements OnInit, OnDestroy {
+    modal?: NgbModalRef;
+    constructor(private modalService: NgbModal) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    open(): void {
+        this.modal = this.modalService.open(ModalAddOrderComponent, {backdrop: 'static'});
+        this.modal.result.then(
+            (result) => {
+                console.log(result);
+            },
+            (reason) => {
+                console.log(reason);
+            }
+        );
+    }
 
+    ngOnDestroy(): void {
+        this.modal?.dismiss();
+    }
 }
