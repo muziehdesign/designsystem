29949d81c3fedd6e72f885ccc275670bdfff2c76
Ruifang Li <ruifang.li09@gmail.com>
2022-02-21 09:13:07 -0700

    remove unused imports

--- src/app/examples/resource-list/resource-list.component.spec.ts
+++ src/app/examples/resource-list/resource-list.component.spec.ts
@@ -4,14 +4,8 @@ import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 
 import { ResourceListComponent } from './resource-list.component';
 import { ResultTableComponent, PaginationComponent } from 'muzieh-ngcomponents';
-import { ResultTableHarness } from 'projects/components/src/lib/result-table/testing/result-table-harness';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
-import { createComponentFactory, Spectator } from '@ngneat/spectator';
-import { DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
-import { ResourceService } from '../resource.service';
-import { of } from 'rxjs';
 
 fdescribe('ResourceListComponent', () => {
     let component: ResourceListComponent;
