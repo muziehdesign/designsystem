18673c52f1f110921b9ea6dbd701447772b83ceb
ruifang <ruifang.li09@gmail.com>
2022-03-05 18:35:04 -0700

    version script to take build number and tag

--- version.sh
+++ version.sh
@@ -1,12 +1,15 @@
-# first argument = build number
-# second argument = prefix
-
+#!/bin/sh
 BUILD=$1
 PREFIX=$2
 
+if [ -n "${2}" ] 
+then
+    PREFIX="${PREFIX}."
+    fi
+
 VERSION=$(npm pkg get version)
 VERSION=${VERSION::-1}
-VERSION="${VERSION}-${BUILD}"
+VERSION="${VERSION}-${PREFIX}${BUILD}"
 VERSION=${VERSION:1}
 echo $VERSION
 npm version "${VERSION}" --no-commit-hooks --no-git-tag-version
