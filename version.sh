f2681ba0ee6b1e65f3089471ddf2b2471225f7c9
ruifang <ruifang.li09@gmail.com>
2022-03-05 18:20:02 -0700

    version script

--- version.sh
+++ version.sh
@@ -1,1 +1,12 @@
-npm version 1.0.0 --json
+# first argument = build number
+# second argument = prefix
+
+BUILD=$1
+PREFIX=$2
+
+VERSION=$(npm pkg get version)
+VERSION=${VERSION::-1}
+VERSION="${VERSION}-${BUILD}"
+VERSION=${VERSION:1}
+echo $VERSION
+npm version "${VERSION}" --no-commit-hooks --no-git-tag-version
