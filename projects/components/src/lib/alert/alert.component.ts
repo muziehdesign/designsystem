129e86679c353959e41cd474e1a659f7dacd83ea
aartavia <aartavia@enerbankusa.com>
2022-03-11 11:55:09 -0600

    changes

--- projects/components/src/lib/alert/alert.component.ts
+++ projects/components/src/lib/alert/alert.component.ts
@@ -1,36 +1,35 @@
 import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 
 @Component({
-  selector: 'mz-alert',
-  templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css']
+    selector: 'mz-alert',
+    templateUrl: './alert.component.html',
+    styleUrls: ['./alert.component.css'],
 })
 export class AlertComponent implements OnInit {
-  @Input() title: string = '';
-  @Input() type: 'info' | 'success' | 'error' | 'warning' = 'info';
-  @Input() body: TemplateRef<any> | undefined | null;
-  @Input() footer: TemplateRef<any> | undefined | null;
+    @Input() title: string = '';
+    @Input() type: 'info' | 'success' | 'error' | 'warning' = 'info';
+    @Input() body: TemplateRef<any> | undefined | null;
+    @Input() footer: TemplateRef<any> | undefined | null;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
-  getAlertClass(): string {
-    return `alert-${this.type}`;
-  }
-
-  getAlertIcon(): string {
-    let icon = '';
-    if (this.type === 'info') {
-      icon = 'information-circle';
-    } else if (this.type === 'success') {
-      icon = 'check-circle';
-    } else if (this.type === 'error') {
-      icon = 'x-circle';
-    } else if (this.type === 'warning') {
-      icon = 'exclamation';
+    getAlertClass(): string {
+        return `alert-${this.type}`;
     }
-    return icon;
-  }
+
+    getAlertIcon(): string {
+        let icon = '';
+        if (this.type === 'info') {
+            icon = 'information-circle';
+        } else if (this.type === 'success') {
+            icon = 'check-circle';
+        } else if (this.type === 'error') {
+            icon = 'x-circle';
+        } else if (this.type === 'warning') {
+            icon = 'exclamation';
+        }
+        return icon;
+    }
 }
