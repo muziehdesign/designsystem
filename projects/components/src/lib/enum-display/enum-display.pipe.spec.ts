7a9ca59ef0e4e5d53f7b8f26ff2b53995c39bb15
Alvaro Artavia <alvartabe777@gmail.com>
2022-03-17 16:08:11 -0600

    pipe update (#30)
    Co-authored-by: aartavia <aartavia@enerbankusa.com>

--- projects/components/src/lib/enum-display/enum-display.pipe.spec.ts
+++ projects/components/src/lib/enum-display/enum-display.pipe.spec.ts
@@ -1,8 +1,27 @@
 import { EnumDisplayPipe } from './enum-display.pipe';
 
 describe('EnumDisplayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EnumDisplayPipe();
-    expect(pipe).toBeTruthy();
-  });
+    let pipe: EnumDisplayPipe;
+
+    beforeEach(() => {
+        pipe = new EnumDisplayPipe();
+    });
+
+    it('create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+
+    it(`should transform values correctly`, () => {
+        expect(pipe.transform('')).toBe('');
+        expect(pipe.transform('t')).toBe('T');
+        expect(pipe.transform('ABCD')).toBe('A b c d');
+        expect(pipe.transform('test')).toBe('Test');
+        expect(pipe.transform('testA')).toBe('Test a');
+        expect(pipe.transform('testAB')).toBe('Test a b');
+        expect(pipe.transform('testValue')).toBe('Test value');
+        expect(pipe.transform('TestValue')).toBe('Test value');
+        expect(pipe.transform('aAbBcCdD')).toBe('A ab bc cd d');
+        expect(pipe.transform('partiallyFulfilled')).toBe('Partially fulfilled');
+        expect(pipe.transform('testValueSecondTry')).toBe('Test value second try');
+    });
 });
