31272e686611ad0583d4cff70383e493554af701
Mauricio Araica <mauricioara.94@gmail.com>
2022-03-22 19:22:21 -0600

    Include date time component in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AlertComponent, EnumDisplayPipe, MuziehComponentsModule, PhoneNumberPipe, DateDisplayPipe, ResultTableComponent, SpinnerComponent, SvgIconComponent } from 'muzieh-ngcomponents';
+import { AlertComponent, EnumDisplayPipe, MuziehComponentsModule, PhoneNumberPipe, DateDisplayPipe, ResultTableComponent, SpinnerComponent, SvgIconComponent, DatetimeComponent } from 'muzieh-ngcomponents';
 
 @NgModule({
-    exports: [SvgIconComponent, SpinnerComponent, EnumDisplayPipe, DateDisplayPipe, ResultTableComponent, PhoneNumberPipe, AlertComponent],
+    exports: [SvgIconComponent, SpinnerComponent, EnumDisplayPipe, DateDisplayPipe, ResultTableComponent, PhoneNumberPipe, DatetimeComponent],
     imports: [MuziehComponentsModule, CommonModule],
 })
 export class SharedModule {}
