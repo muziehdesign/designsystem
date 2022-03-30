06480006bbe975acdebffd51a20161728279516d
ruifang <ruifang.li09@gmail.com>
2022-03-29 19:30:48 -0600

    fix filename

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ BRANCH=$1
 
 if [ "${1}" != "develop" ]
 then
-    npm publish ./dist/components --tag latest
-else
     npm publish ./dist/components
+else
+    npm publish ./dist/components --tag latest
     fi
