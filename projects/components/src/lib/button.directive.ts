042467565ee2bb0c8f1e86d75df08ddc80f60f97
Juan Gomez <jgomez@enerbankusa.com>
2022-03-30 14:53:53 -0600

    [PP-2081] use svg

--- projects/components/src/lib/button.directive.ts
+++ projects/components/src/lib/button.directive.ts
@@ -14,6 +14,16 @@ export class ButtonDirective implements OnChanges {
     @Input()
     busy?: boolean;
     private loadingElement?: Node;
+    private svgContent = `
+    <svg class="animate-spin m-auto h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
+        viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+        </circle>
+        <path class="opacity-75" fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+        </path>
+    </svg>
+    `;
 
     constructor(private renderer: Renderer2, private hostElement: ElementRef) {}
 
@@ -44,8 +54,8 @@ export class ButtonDirective implements OnChanges {
         if (this.busy) {
             this.loadingElement = this.renderer.createElement('div') as Node;
             const innerSpinner = this.renderer.createElement('div');
+            innerSpinner.innerHTML = this.svgContent;
             this.renderer.addClass(this.loadingElement, 'button-loading-overlay');
-            this.renderer.addClass(innerSpinner, 'spinner');
             this.renderer.appendChild(this.loadingElement, innerSpinner);
             this.renderer.appendChild(this.hostElement.nativeElement, this.loadingElement);
         } else {
