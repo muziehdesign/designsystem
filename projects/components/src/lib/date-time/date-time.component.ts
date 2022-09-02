ec4f67868bb0fb9547a6dc2eed759f3c4edff2d2
bserranocr <105395872+bserranocr@users.noreply.github.com>
2022-09-01 18:19:46 -0600

    [PQ-1130] disable inputs when required (#40)
    * [PQ-1130] disable inputs when required
    
    * [PQ-1130] fixing css style
    
    * [PQ-1130] css tweak
    
    * [PQ-1130] fix
    
    * [PQ-1130] fixing bug with time update and typo
    
    * [PQ-1130] pr comment
    
    * [PQ-1130] testing and checking change
    
    * [PQ-1130] removing consoles

--- projects/components/src/lib/date-time/date-time.component.ts
+++ projects/components/src/lib/date-time/date-time.component.ts
@@ -5,83 +5,96 @@ import { TimeValue } from './time-value';
 import { MatDatepicker } from '@angular/material/datepicker';
 
 @Component({
-  selector: 'mz-datetime',
-  templateUrl: './date-time.component.html',
-  styleUrls: ['./date-time.component.css'],
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => DateTimeComponent),
-    multi: true
-  }]
+    selector: 'mz-datetime',
+    templateUrl: './date-time.component.html',
+    styleUrls: ['./date-time.component.css'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => DateTimeComponent),
+            multi: true,
+        },
+    ],
 })
 export class DateTimeComponent implements ControlValueAccessor {
+    date: Date | undefined;
+    time: String | undefined;
+    private tempDate: string | undefined | null = null;
+    private tempTime: string | null = null;
+    type = 'outline';
+    @ViewChild('datePicker') datepicker!: MatDatepicker<Date>;
 
-  date: Date | undefined;
-  time: String | undefined;
-  private tempDate: string | undefined | null = null;
-  private tempTime: string | null = null;
-  type = 'outline';
-  @ViewChild('datePicker') datepicker!: MatDatepicker<Date>;
+    @Input()
+    min?: Date | undefined;
 
-  @Input()
-  min?: Date | undefined;
+    @Input()
+    max?: Date | undefined;
 
-  @Input()
-  max?: Date | undefined;
+    @Input()
+    disabled = false;
 
-  constructor(private _cd: ChangeDetectorRef) { }
+    constructor(private _cd: ChangeDetectorRef) {}
 
-  onChange = (_: any) => {};
-  onTouched = () => {};
+    onChange = (_: any) => {};
+    onTouched = () => {};
 
-  writeValue(value: Date) {
-    if (value) {
-      this.date = value;
-      const hourPrefix = value.getHours() > 9 ? '' : '0';
-      const minutePrefix = value.getMinutes() > 9 ? '' : '0';
-      this.time = `${hourPrefix}${value.getHours()}:${minutePrefix}${value.getMinutes()}`;
-      this._cd.markForCheck();
+    writeValue(value: Date) {
+        if (value) {
+            this.date = value;
+            const hourPrefix = value.getHours() > 9 ? '' : '0';
+            const minutePrefix = value.getMinutes() > 9 ? '' : '0';
+            this.time = `${hourPrefix}${value.getHours()}:${minutePrefix}${value.getMinutes()}`;
+            this._cd.markForCheck();
+        }
     }
-  }
 
-  registerOnChange(fn: (value: any) => any): void { this.onChange = fn; }
-
-  registerOnTouched(fn: any): void { this.onTouched = fn; }
-
-  updateDate(val: any): void {
-    this.tempDate = val.target.value.toString();
-    this.propagateModelCahnge();
-  }
-
-  updateTime(val: string):void {
-    this.tempTime = val.toString();
-    this.propagateModelCahnge();
-  }
-
-  updateDateEvent(event: MatDatepickerInputEvent<Date>) {
-    let value;
-    if (event.value) {
-      value = event.value;
-      const date = `${value.getMonth() + 1}/${value.getDate()}/${value.getFullYear()}`;
-      this.tempDate = date;
-      this.propagateModelCahnge();
+    registerOnChange(fn: (value: any) => any): void {
+        this.onChange = fn;
     }
-  }
 
-  focusOnDateInput() {
-    this.datepicker.open();
-  }
-
-  private propagateModelCahnge(): void {
-    if (this.tempDate && this.tempTime) {
-      const dateString = `${this.tempDate} ${this.tempTime}`;
-      this.onChange(new Date(dateString));
-    } else {
-      this.onChange(null);
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
     }
-  }
+
+    updateDate(val: any): void {
+        this.tempDate = val.target.value.toString();
+        this.propagateModelChange();
+    }
+
+    updateTime(val: string): void {
+        if (!this.tempDate && this.date) {
+            this.date = new Date(this.date);
+            const date = `${this.date.getMonth() + 1}/${this.date.getDate()}/${this.date.getFullYear()}`;
+            this.tempDate = date;
+        }
+        this.tempTime = val.toString();
+        this.propagateModelChange();
+    }
+
+    updateDateEvent(event: MatDatepickerInputEvent<Date>) {
+        let value;
+        if (event.value) {
+            value = event.value;
+            const date = `${value.getMonth() + 1}/${value.getDate()}/${value.getFullYear()}`;
+            this.tempDate = date;
+            this.propagateModelChange();
+        }
+    }
+
+    focusOnDateInput() {
+        this.datepicker.open();
+    }
+
+    private propagateModelChange(): void {
+        if (this.tempTime) {
+            const dateString = `${this.tempDate} ${this.tempTime}`;
+            this.onChange(new Date(dateString));
+        } else {
+            this.onChange(null);
+        }
+    }
 }
 
 export interface ScheduleInputModel {
-  time: TimeValue
+    time: TimeValue;
 }
