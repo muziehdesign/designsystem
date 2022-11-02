26e40614dfce288450f10e811160e155e51f254f
Ruifang Li <ruifang@users.noreply.github.com>
2022-11-02 09:33:02 -0600

    delete spec

--- src/app/shared/embed.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EmbedDirective } from './embed.directive';
-
-xdescribe('EmbedDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new EmbedDirective();
-  //   expect(directive).toBeTruthy();
-  // });
-});
