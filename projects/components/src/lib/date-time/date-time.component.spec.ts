6b4d800145cf301719c2917bd59255c3b378770d
Paul Free <paulfree@paulfree.com>
2023-04-11 08:26:35 -0400

    Added Unit tests

--- projects/components/src/lib/date-time/date-time.component.spec.ts
+++ projects/components/src/lib/date-time/date-time.component.spec.ts
@@ -7,7 +7,7 @@ import { MuziehDateTimeModule } from './date-time.module';
 
 @Component({ selector: 'test-cmp', template: '' })
 class TestComponent {
-    dateTime: Date | undefined;
+    dateTime: Date | undefined | null;
 }
 
 function createGenericTestComponent<T>(html: string, type: { new (...args: any[]): T }, detectChanges = true): ComponentFixture<T> {
@@ -36,8 +36,12 @@ function expectToDisplayDateTime(el: HTMLElement, time: string, date: string) {
 }
 
 function convertDate(dateValue: string) {
+    if (!dateValue) {
+        return '';
+    }
+
     const date = new Date(dateValue);
-    return `${date.getMonth()}/${date.getUTCDate()}/${date.getFullYear()}`;
+    return `${date.getMonth() + 1}/${date.getUTCDate()}/${date.getFullYear()}`;
 }
 
 describe('DatetimeComponent', () => {
@@ -58,7 +62,7 @@ describe('DatetimeComponent', () => {
             fixture.detectChanges();
             tick();
             fixture.detectChanges();
-            expectToDisplayDateTime(fixture.nativeElement, '16:20', '11/12/2022');
+            expectToDisplayDateTime(fixture.nativeElement, '16:20', '12/12/2022');
         }));
 
         it('should update inputs value on model change', fakeAsync(() => {
@@ -70,13 +74,101 @@ describe('DatetimeComponent', () => {
             fixture.detectChanges();
             tick();
             fixture.detectChanges();
-            expectToDisplayDateTime(fixture.nativeElement, '13:50', '11/12/2022');
+            expectToDisplayDateTime(fixture.nativeElement, '13:50', '12/12/2022');
 
             fixture.componentInstance.dateTime = new Date('09/11/2022 09:10');
             fixture.detectChanges();
             tick();
             fixture.detectChanges();
-            expectToDisplayDateTime(fixture.nativeElement, '09:10', '8/11/2022');
+            expectToDisplayDateTime(fixture.nativeElement, '09:10', '9/11/2022');
+        }));
+
+        it('should clear inputs value when null/undefined model', fakeAsync(() => {
+            const html = `<mz-datetime [(ngModel)]="dateTime"></mz-datetime>`;
+
+            const fixture = createTestComponent(html);
+            fixture.componentInstance.dateTime = new Date('12/12/2022 13:50');
+            fixture.detectChanges();
+            tick();
+            fixture.detectChanges();
+            expectToDisplayDateTime(fixture.nativeElement, '13:50', '12/12/2022');
+
+            fixture.componentInstance.dateTime = null;
+            fixture.detectChanges();
+            tick();
+            fixture.detectChanges();
+           
+            expectToDisplayDateTime(fixture.nativeElement, '', '');
+        }));
+        
+        it('should clear time input when clear date input', fakeAsync(() => {
+            const html = `<mz-datetime [(ngModel)]="dateTime"></mz-datetime>`;
+
+            const fixture = createTestComponent(html);
+            fixture.componentInstance.dateTime = new Date('12/12/2022 13:50');
+            fixture.detectChanges();
+            tick();
+            fixture.detectChanges();
+            expectToDisplayDateTime(fixture.nativeElement, '13:50', '12/12/2022');
+
+            let inputs = getInputs(fixture.nativeElement);
+
+            inputs[0].value = '';
+            inputs[0].dispatchEvent(new InputEvent('change'));
+
+            fixture.detectChanges();
+           
+            expect(inputs[1].value).toEqual('')
+        }));
+
+        it('should clear date input when clear time input', fakeAsync(() => {
+            const html = `<mz-datetime [(ngModel)]="dateTime"></mz-datetime>`;
+
+            const fixture = createTestComponent(html);
+            fixture.componentInstance.dateTime = new Date('12/12/2022 13:50');
+            fixture.detectChanges();
+            tick();
+            fixture.detectChanges();
+            expectToDisplayDateTime(fixture.nativeElement, '13:50', '12/12/2022');
+
+            let inputs = getInputs(fixture.nativeElement);
+
+            inputs[1].value = '';
+            inputs[1].dispatchEvent(new InputEvent('change'));
+
+            fixture.detectChanges();
+           
+            expect(inputs[0].value).toEqual('');
+        }));
+
+        it('should populate time when enter date', fakeAsync(() => {
+            const html = `<mz-datetime [(ngModel)]="dateTime"></mz-datetime>`;
+
+            const fixture = createTestComponent(html);
+
+            let inputs = getInputs(fixture.nativeElement);
+
+            inputs[0].value = '12/12/2022';
+            inputs[0].dispatchEvent(new InputEvent('change'));
+
+            fixture.detectChanges();
+           
+            expect(inputs[1].value).toBeTruthy();
+        }));
+
+        it('should populate date when enter time', fakeAsync(() => {
+            const html = `<mz-datetime [(ngModel)]="dateTime"></mz-datetime>`;
+
+            const fixture = createTestComponent(html);
+
+            let inputs = getInputs(fixture.nativeElement);
+
+            inputs[1].value = '09:15';
+            inputs[1].dispatchEvent(new InputEvent('change'));
+
+            fixture.detectChanges();
+           
+            expect(inputs[0].value).toBeTruthy();
         }));
     });
 });
