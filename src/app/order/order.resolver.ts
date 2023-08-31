00b188a6def98eb9238bef0ade55d4e85dbb1677
ruifang <ruifang.li09@gmail.com>
2023-08-31 17:24:08 -0600

    core angular packages @16

--- src/app/order/order.resolver.ts
+++ src/app/order/order.resolver.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@angular/core';
-import {
-  Router, Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
+import { Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { OrderModel } from './models';
 import { OrderService } from './order.service';
 
 @Injectable()
-export class OrderResolver implements Resolve<OrderModel> {
+export class OrderResolver  {
   constructor(private serivce: OrderService){}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<OrderModel> {
     return this.serivce.getOrder(route.params['orderId'] as number);
