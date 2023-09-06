003f9d5d27a82f40fdfd110340acbcf8226b5fa2
ruifang <ruifang.li09@gmail.com>
2023-09-06 14:15:07 -0600

    add design

---
+++ projects/design/src/lib/design.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { DesignComponent } from './design.component';
+
+
+
+@NgModule({
+  declarations: [
+    DesignComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    DesignComponent
+  ]
+})
+export class DesignModule { }
---
+++ projects/design/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of design
+ */
+
+export * from './lib/design.service';
+export * from './lib/design.component';
+export * from './lib/design.module';
