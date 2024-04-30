36657772f0cae67b50caec49d6c56a43483a0f00
ruifang <ruifang.li09@gmail.com>
2024-04-30 11:52:23 -0600

    add new components library

---
+++ projects/muziehdesign/components/src/lib/components.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-components',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      components works!
+    </p>
+  `,
+  styles: ``
+})
+export class ComponentsComponent {
+
+}
