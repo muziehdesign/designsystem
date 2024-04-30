be7029f2032491645fc064a82a2ef724fc310066
ruifang <ruifang.li09@gmail.com>
2024-04-30 11:53:06 -0600

    add new design system library

---
+++ projects/muziehdesign/designsystem/src/lib/designsystem.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { DesignsystemComponent } from './designsystem.component';
+
+
+
+@NgModule({
+  declarations: [
+    DesignsystemComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    DesignsystemComponent
+  ]
+})
+export class DesignsystemModule { }
