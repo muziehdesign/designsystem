158a08f6a03a2c10c73b391f37f057785d351a36
ruifang <ruifang.li09@gmail.com>
2024-06-26 11:23:29 -0600

    css fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,9 +32,7 @@ module.exports = {
     },
     {
       pattern: /text-(red|green|blue|gray|yellow)-(50|100|200|300|400|500|600|700|800|900)/,
-    },
-    'nav-item',
-    'nav-link'
+    }
   ],
   plugins: ['postcss-import', require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
