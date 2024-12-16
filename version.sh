4c5003b35922c346d0d206130623b79e1da6b6a9
ruifang <ruifang.li09@gmail.com>
2024-12-16 10:37:45 -0700

    test storing version numbers

--- version.sh
+++ version.sh
@@ -8,9 +8,18 @@ MAJOR=$(echo "$VERSION" | awk -F '[/.]' '{ print $1 }')
 MINOR=$(echo "$VERSION" | awk -F '[/.]' '{ print $2 }')
 PATCH=$BUILD
 
-if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "develop" ] ||  [[ "$BRANCH" = \release\/* ]]
+PACKAGEVERSION=$VERSION
+TAG=$MAJOR
+
+if [ "$BRANCH" = "next" ] || [ "$BRANCH" = "develop" ] ||  [[ "$BRANCH" = \release\/* ]]
 then
+    PACKAGEVERSION="${MAJOR}.${MINOR}.${PATCH:="0"}"
     npm version "${MAJOR}.${MINOR}.${PATCH:="0"}" --no-commit-hooks --no-git-tag-version
 else
+    PACKAGEVERSION="${MAJOR}.${MINOR}.${PATCH:="0"}-beta"
+    TAG="beta"
     npm version "${MAJOR}.${MINOR}.${PATCH:="0"}-beta" --no-commit-hooks --no-git-tag-version
 fi
+
+echo "$PACKAGEVERSION" > foo.txt
+echo "$TAG" >> foo.txt
