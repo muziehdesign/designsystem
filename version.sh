113e22fda4bfcd94198e2f05c7d8814828debbd1
ruifang <ruifang.li09@gmail.com>
2024-12-16 10:45:54 -0700

    debug

--- version.sh
+++ version.sh
@@ -11,15 +11,18 @@ PATCH=$BUILD
 PACKAGEVERSION=$VERSION
 TAG=$MAJOR
 
-if [ "$BRANCH" = "next" ] || [ "$BRANCH" = "develop" ] ||  [[ "$BRANCH" = \release\/* ]]
+if [ "$BRANCH" = "next" ] || [ "$BRANCH" = "develop" ] ||  [ "$BRANCH" = \release\/* ]
 then
     PACKAGEVERSION="${MAJOR}.${MINOR}.${PATCH:="0"}"
-    npm version "${MAJOR}.${MINOR}.${PATCH:="0"}" --no-commit-hooks --no-git-tag-version
+    #npm version "${MAJOR}.${MINOR}.${PATCH:="0"}" --no-commit-hooks --no-git-tag-version
 else
     PACKAGEVERSION="${MAJOR}.${MINOR}.${PATCH:="0"}-beta"
     TAG="beta"
-    npm version "${MAJOR}.${MINOR}.${PATCH:="0"}-beta" --no-commit-hooks --no-git-tag-version
+    #npm version "${MAJOR}.${MINOR}.${PATCH:="0"}-beta" --no-commit-hooks --no-git-tag-version
 fi
+
+echo "PACKAGEVERSION=$PACKAGEVERSION"
+echo "TAG=$TAG"
 
 echo "$PACKAGEVERSION" > foo.txt
 echo "$TAG" >> foo.txt
