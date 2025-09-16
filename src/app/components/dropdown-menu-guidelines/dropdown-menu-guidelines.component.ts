a839a19112d2448cc43e24a6af08cac6fdd8e523
ruifang <ruifang.li09@gmail.com>
2025-09-16 15:10:19 -0600

    clean up

--- src/app/components/dropdown-menu-guidelines/dropdown-menu-guidelines.component.ts
+++ src/app/components/dropdown-menu-guidelines/dropdown-menu-guidelines.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { ButtonDirective, SvgIconComponent } from '@muziehdesign/components';
+import { MzButton, SvgIconComponent } from '@muziehdesign/components';
 import { CdkMenuModule } from '@angular/cdk/menu';
 
 @Component({
     selector: 'app-dropdown-menu-guidelines',
     templateUrl: './dropdown-menu-guidelines.component.html',
     styleUrls: ['./dropdown-menu-guidelines.component.scss'],
-    imports: [CdkMenuModule, ButtonDirective, SvgIconComponent],
+    imports: [CdkMenuModule, MzButton, SvgIconComponent],
 })
 export class DropdownMenuGuidelinesComponent {
     constructor() {}
